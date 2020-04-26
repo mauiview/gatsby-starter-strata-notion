32b18e20757a3e9fe824cc5ddebe86efa9a29c11
Bradley White <bradleywhite@MacBook-Pro-2.sssnet.com>
2020-04-25 23:12:37 -0400

    commit

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -2,24 +2,6 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import { parseImageUrl } from 'notabase/src/utils'
-import { Disqus } from "gatsby-plugin-disqus"
-const PostTemplate = () => {
-  let disqusConfig = {
-    url: `${config.siteUrl+location.pathname}`,
-    identifier: post.id,
-    title: post.title,
-  }
-  return (
-    <>
-      <h1>{post.title}</h1>
-      <CommentCount config={disqusConfig} placeholder={'...'} />
-      /* Post Contents */
-      <Disqus config={disqusConfig} />
-    </>
-  )
-}
-
-export default PostTemplate
 export default ({ data }) => {
   const { posts: { title, tags, publish_date, html, url, slug, desc, color, cover_image } } = data
 
