7d24224312ccd2f893398d5a1716e3fc0642e836
Bradley White <bradleywhite@Brads-MacBook-Pro.local>
2020-12-06 20:15:46 -0500

    committ

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -15,7 +15,25 @@ export default ({ data }) => {
     </Layout>
       )
     }
+    import { Disqus, CommentCount } from 'gatsby-plugin-disqus'
+
+    const PostTemplate = () => {
+      let disqusConfig = {
+        url: `${config.siteUrl+location.pathname}`,
+        identifier: post.id,
+        title: post.title,
+      }
+      return (
+        <>
+          <h1>{post.title}</h1>
+          <CommentCount config={disqusConfig} placeholder={'...'} />
+          /* Post Contents */
+          <Disqus config={disqusConfig} />
+        </>
+      )
+    }
     
+    export default PostTemplate   
 export const query = graphql`
   query($slug: String!) {
     posts(slug: { eq: $slug }) {
